528aaeb878b839317220db23c31e5aebb16f8aa2
JPStone2022 <julianstone1969@gmail.com>
2025-04-30 15:03:01 +0100

    update build.sh

--- build.sh
+++ build.sh
@@ -36,7 +36,7 @@ echo "Importing/Updating data from CSV..."
 
 # Example for topics (update based on slug)
 # Ensure data/topics.csv is committed to your Git repo
-python manage.py import_data data/topics.csv --model_type topics --update --unique_field slug
+# python manage.py import_data data/topics.csv --model_type topics --update --unique_field slug
 
 # Example for certificates (update based on title)
 # Ensure data/certificates.csv is committed to your Git repo
