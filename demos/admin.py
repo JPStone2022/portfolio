03e411419ea5c5ae119f112d449f0569bba87b07
JPStone2022 <97758413+JPStone2022@users.noreply.github.com>
2025-05-01 14:21:15 +0100

    update demos

--- demos/admin.py
+++ demos/admin.py
@@ -1,13 +1,13 @@
-# # demos/admin.py
-# from django.contrib import admin
-# from .models import Demo
+# demos/admin.py
+from django.contrib import admin
+from .models import Demo
 
-# @admin.register(Demo)
-# class DemoAdmin(admin.ModelAdmin):
-#     list_display = ('title', 'demo_url_name', 'order', 'is_featured')
-#     list_filter = ('is_featured',)
-#     search_fields = ('title', 'description')
-#     list_editable = ('order', 'is_featured')
-#     prepopulated_fields = {'slug': ('title',)}
-#     fields = ('title', 'slug', 'description', 'demo_url_name', 'image_url', 'order', 'is_featured')
+@admin.register(Demo)
+class DemoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'demo_url_name', 'order', 'is_featured')
+    list_filter = ('is_featured',)
+    search_fields = ('title', 'description')
+    list_editable = ('order', 'is_featured')
+    prepopulated_fields = {'slug': ('title',)}
+    fields = ('title', 'slug', 'description', 'demo_url_name', 'image_url', 'order', 'is_featured')
 
