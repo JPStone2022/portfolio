0fb0ff7d2b5386cc677fdd3fbf70df501f44e58f
JPStone2022 <97758413+JPStone2022@users.noreply.github.com>
2025-05-01 16:22:47 +0100

    update settings for demo

--- dl_portfolio_project/settings.py
+++ dl_portfolio_project/settings.py
@@ -44,10 +44,10 @@ DEBUG = os.environ.get('DJANGO_DEBUG', 'False') == 'True' # Default to True for 
 #DEBUG = 'False'
 # Update ALLOWED_HOSTS based on environment
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '127.0.0.1 localhost').split(' ')
-#ALLOWED_HOSTS = [os.environ.get('PYTHONANYWHERE_DOMAIN')] # Add your domain/IP in production
-RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
-if RENDER_EXTERNAL_HOSTNAME:
-    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
+ALLOWED_HOSTS = [os.environ.get('PYTHONANYWHERE_DOMAIN')] # Add your domain/IP in production
+# RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+# if RENDER_EXTERNAL_HOSTNAME:
+#     ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 # Add your custom domain here if you set one up later
 # CUSTOM_DOMAIN = os.environ.get('CUSTOM_DOMAIN')
 # if CUSTOM_DOMAIN:
